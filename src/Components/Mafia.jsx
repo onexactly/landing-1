32497aedd2ae79b6faf5f8a6031f7242e6d3c5be
Nurmukhammad <abdu1rahimovv@gmail.com>
2023-01-29 19:17:30 +0500

    (fixed): resolve some warnings

--- src/Components/Mafia.jsx
+++ src/Components/Mafia.jsx
@@ -14,11 +14,11 @@ function Mafia() {
   window.addEventListener("scroll", changeBackground);
 
   return (
-    <nav className={nav ? "nav actic" : "nav"}>
+    <nav className={nav ? 'nav active' : 'nav'}>
       <a href="/" className="logo">
         <img src={logo} alt="" />
       </a>
-      <input type="checkbox" className="menu-btn" />
+      <input type="checkbox" className="menu-btn" id='menu-btn' />
       <label className="menu-icon" for="menu-btn">
         <span className="nav-icon"></span>
       </label>
